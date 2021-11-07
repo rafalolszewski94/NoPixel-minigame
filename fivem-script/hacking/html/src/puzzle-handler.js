4e22f7eae14bec3d9092eb3cdc44b0dd05542be1
Rafał Olszewski <me@orafal.dev>
2021-11-07 15:53:34 +0100

    Readable code

--- fivem-script/hacking/html/src/puzzle-handler.js
+++ fivem-script/hacking/html/src/puzzle-handler.js
@@ -1,84 +1,93 @@
-import { $, shuffleArray, delay, playSound } from './helpers.js'
-import { generateRandomPuzzle, generateQuestionAndAnswer } from './puzzle-factory.js'
-import { getPuzzleSvg } from './svg-factory.js'
+import { $, shuffleArray, delay, playSound } from "./helpers.js";
+import {
+  generateRandomPuzzle,
+  generateQuestionAndAnswer,
+} from "./puzzle-factory.js";
+import { getPuzzleSvg } from "./svg-factory.js";
 
-const progressBar = $('.answer-progress-bar')
-const inputElement = $('.answer-input')
+const progressBar = $(".answer-progress-bar");
+const inputElement = $(".answer-input");
 
-let puzzleTime = 7
-let puzzleAmount = 4
+let puzzleTime = 7;
+let puzzleAmount = 4;
 
 // handles generating puzzle and returning result
-export async function doPuzzle(){
-    // reset from previous run
-    $('.answer-section').classList.add('hidden')
-    $(".number-container").innerHTML = ''
+export async function doPuzzle() {
+  // reset from previous run
+  $(".answer-section").classList.add("hidden");
+  $(".number-container").innerHTML = "";
 
-    //Generate squares and puzzles
-    const squares = [...Array(puzzleAmount).keys()].map(i => {
-        let square = document.createElement('div')
-        square.id = `square-${i+1}`
-        square.className = 'square'
-        $('#number-container').appendChild(square)
-        return square
-    })
-    const puzzles = [...Array(puzzleAmount)].map(_ => generateRandomPuzzle())
-      
-    // generate numbers and display
-    const nums = shuffleArray([...Array(puzzleAmount)].map((v, i) => i+1))
-    await displayNumbers(nums)
+  //Generate squares and puzzles
+  const squares = [...Array(puzzleAmount).keys()].map((i) => {
+    let square = document.createElement("div");
+    square.id = `square-${i + 1}`;
+    square.className = "square";
+    $("#number-container").appendChild(square);
+    return square;
+  });
+  const puzzles = [...Array(puzzleAmount)].map((_) => generateRandomPuzzle());
 
-    const metronome = playSound('assets/metronome.mp3')
+  // generate numbers and display
+  const nums = shuffleArray([...Array(puzzleAmount)].map((v, i) => i + 1));
+  await displayNumbers(nums);
 
-    // clear and focus input window
-    $('.answer-section').classList.remove('hidden')
-    inputElement.value = ''
-    inputElement.focus()
+  const metronome = playSound("assets/metronome.mp3");
 
-    // activate time remaining countdown bar 
-    
-    progressBar.style.transition = ``
-    progressBar.classList.remove('answer-progress-bar-shrink')
-    await delay(0.1)
-    progressBar.style.transition = `width ${puzzleTime*1000}ms linear`
-    progressBar.classList.add('answer-progress-bar-shrink')
-    
+  // clear and focus input window
+  $(".answer-section").classList.remove("hidden");
+  inputElement.value = "";
+  inputElement.focus();
 
-    // display puzzle in squares
-    squares.forEach((square, i) => square.style.backgroundColor = puzzles[i].colors['background'])
-    squares.forEach((square, i)  => square.innerHTML =  getPuzzleSvg(puzzles[i]))
+  // activate time remaining countdown bar
 
-    // generate and display question
-    const [question, answer] = generateQuestionAndAnswer(nums, puzzles) 
-    $('.answer-question').textContent = question.toUpperCase()
+  progressBar.style.transition = ``;
+  progressBar.classList.remove("answer-progress-bar-shrink");
+  await delay(0.1);
+  progressBar.style.transition = `width ${puzzleTime * 1000}ms linear`;
+  progressBar.classList.add("answer-progress-bar-shrink");
 
-    return new Promise(async (resolve) => {
+  // display puzzle in squares
+  squares.forEach(
+    (square, i) =>
+      (square.style.backgroundColor = puzzles[i].colors["background"])
+  );
+  squares.forEach((square, i) => (square.innerHTML = getPuzzleSvg(puzzles[i])));
 
-        // return written input and answer
-        inputElement.addEventListener("keyup", (event) => {
-            if (event.keyCode === 13) {
-                metronome.pause()
-                resolve([inputElement.value, answer])
-            }
-        });
+  // generate and display question
+  const [question, answer] = generateQuestionAndAnswer(nums, puzzles);
+  $(".answer-question").textContent = question.toUpperCase();
 
-        // return nothing by default if puzzleTime seconds go by
-        await delay(puzzleTime)
-        metronome.pause()
-        resolve([null, answer])
+  return new Promise(async (resolve) => {
+    // return written input and answer
+    inputElement.addEventListener("keyup", (event) => {
+      if (event.keyCode === 13) {
+        metronome.pause();
+        resolve([inputElement.value, answer]);
+      }
     });
+
+    // return nothing by default if puzzleTime seconds go by
+    await delay(puzzleTime);
+    metronome.pause();
+    resolve([null, answer]);
+  });
 }
 
+async function displayNumbers(numbers) {
+  numbers.forEach(
+    (n, i) =>
+      ($(
+        "#square-" + (i + 1)
+      ).innerHTML = `<div class="big-numbers can-shrink" id="num-${
+        i + 1
+      }">${n}</div>`)
+  );
 
-async function displayNumbers(numbers){
-    
-    numbers.forEach((n, i) => $('#square-' + (i+1)).innerHTML = `<div class="big-numbers can-shrink" id="num-${i+1}">${n}</div>`)
-
-    await delay(1.5)
-    numbers.forEach(n => $('#num-' + (n)).classList.add('number-shrink'))
-    await delay(1.5)
+  await delay(1.5);
+  numbers.forEach((n) => $("#num-" + n).classList.add("number-shrink"));
+  await delay(1.5);
 }
 
 // puzzle time settins
-const timeRange = 7
-const puzzleRange = 4
+const timeRange = 7;
+const puzzleRange = 4;
